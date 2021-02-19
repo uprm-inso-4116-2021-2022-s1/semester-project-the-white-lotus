cbf06bc664ca03a44453e5d4962e57b0ad33970d
HectorMiranda <hector.miranda8@upr.edu>
2021-02-19 17:36:16 -0400

    complete example API

--- lftv-backend/app.js
+++ lftv-backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require ('mysql');
+const bodyParser = require('body-parser');
 
 // Create connection
 const db = mysql.createConnection({
@@ -10,7 +11,7 @@ const db = mysql.createConnection({
     insecureAuth : true
 });
 
-// Connect
+// Connect to database
 db.connect((err) => {
    if (err)
        throw err;
@@ -18,6 +19,10 @@ db.connect((err) => {
 });
 
 const app = express();
+// required for requests to have json body
+const jsonParser = bodyParser.json();
+//required for requests to have x-www-form-urlencoded body
+const urlencodedParser = bodyParser.urlencoded({ extended : false});
 
 // Create DB
 // to create the db, uncomment the following lines and remove from above "database: 'lftv'," temporarily
@@ -32,6 +37,8 @@ const app = express();
 // });
 
 
+//#region Example API
+// ******************************** EXAMPLE API ****************************************************
 
 // Create table
 app.get('/createpoststable', (req, res) => {
@@ -44,7 +51,7 @@ app.get('/createpoststable', (req, res) => {
 });
 
 // Insert post
-app.get('/addpost1', (req, res) => {
+app.get('/addpost', (req, res) => {
     let post = {title: 'Post One', body: 'This is a random post'};
     let sql = 'INSERT INTO posts SET ?';
     let query = db.query(sql, post, (err, result) => {
@@ -54,14 +61,33 @@ app.get('/addpost1', (req, res) => {
     });
 });
 
+// Insert post with body
+app.get('/addpostbody', jsonParser, (req, res) => {
+    let post = {title: req.body.title, body: req.body.body};
+    let sql = 'INSERT INTO posts SET ?';
+    let query = db.query(sql, post, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send({
+            message: 'Post added...',
+            post
+        });
+    });
+    // console.log(req);
+    // res.send('Done')
+});
+
 // Select posts
 app.get('/getposts', (req, res) => {
     let sql = 'SELECT * FROM posts';
     db.query(sql, (err, results) => {
         if(err) throw err;
         console.log(results);
-        res.send(`Posts fetched...\n` + results.toString());
-    })
+        res.send({
+            message: 'Posts feteched...',
+            results
+        });
+    });
 });
 
 // Select individual post
@@ -70,9 +96,35 @@ app.get('/getpost/:id', (req, res) => {
     db.query(sql, (err, result) => {
         if(err) throw err;
         console.log(result);
-        res.send(`Post fetched...\n` + result.toString());
+        res.send(`Post fetched...`);
     })
 });
+
+// Update post
+app.get('/updatepost/:id', (req, res) => {
+    let newTitle = 'Updated Title'; // hardcorded change, another can be receiving JSON and using req.body.{whatevs}
+    let sql = `UPDATE posts SET title = '${newTitle}' WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send(`Post updated...`);
+    });
+});
+
+// Remove single post
+app.get('/deletepost/:id', (req, res) => {
+    let sql = `DELETE FROM posts WHERE id = ${req.params.id}`;
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send(`Post deleted...`);
+    })
+});
+
+// ******************************** EXAMPLE API ****************************************************
+//#endregion
+
+
 
 const PORT = process.env.PORT || 3000;
 
