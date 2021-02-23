6e8143f109532167219e7fabebfa9ab637ab31b4
malejandramunoz <maria.munoz10@upr.edu>
2021-02-23 12:13:17 -0400

    Changes to the modal and stuff

--- leavesfromthevine/src/views/TeaCatalogue.js
+++ leavesfromthevine/src/views/TeaCatalogue.js
@@ -19,65 +19,11 @@
 import React from "react";
 
 // reactstrap components
-import { Container, Table, Button, FormGroup, Input, Modal } from 'reactstrap';
+import {Container, Table } from 'reactstrap';
 // core components
 import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
 import DemoFooter from "../components/Footers/DemoFooter";
 import teas from "../dummy data/tea.json";
-
-function TeaModal() {
-    const [liveDemo, setLiveDemo] = React.useState(false);
-    return(
-        <>
-            <Button color="primary" type="button" onClick={() => setLiveDemo(true)}>
-                Launch demo modal
-            </Button>
-            <Modal isOpen={liveDemo} toggle={() => setLiveDemo(false)}>
-                <div className="modal-header">
-                    <h5 className="modal-title" id="exampleModalLiveLabel">
-                        Modal title
-                    </h5>
-                    <button
-                        aria-label="Close"
-                        className="close"
-                        data-dismiss="modal"
-                        type="button"
-                        onClick={() => setLiveDemo(false)}
-                    >
-                        <span aria-hidden={true}>×</span>
-                    </button>
-                </div>
-                <div className="modal-body">
-                    <p>Woohoo, you're reading this text in a modal!</p>
-                </div>
-                <div className="modal-footer">
-                    <div className="left-side">
-                        <Button
-                            className="btn-link"
-                            color="default"
-                            data-dismiss="modal"
-                            type="button"
-                            onClick={() => setLiveDemo(false)}
-                        >
-                            Never mind
-                        </Button>
-                    </div>
-                    <div className="divider" />
-                    <div className="right-side">
-                        <Button
-                            className="btn-link"
-                            color="danger"
-                            type="button"
-                            onClick={() => setLiveDemo(false)}
-                        >
-                            Delete
-                        </Button>
-                    </div>
-                </div>
-            </Modal>
-        </>
-    );
-}
 
 function PageHeader() {
   let pageHeader = React.createRef();
@@ -142,7 +88,7 @@ function TeaCatalogue() {
                         <th>Type</th>
                         <th>Name</th>
                         <th>Description</th>
-                        <th>View More</th>
+                        <th></th>
                     </tr>
                     </thead>
                     <tbody>
@@ -151,7 +97,7 @@ function TeaCatalogue() {
                             <td>{properties.type}</td>
                             <td>{properties.name}</td>
                             <td>{properties.description}</td>
-                            <td><TeaModal /></td>
+                            <td>To be added</td>
                         </tr>
                     )}
                     </tbody>
