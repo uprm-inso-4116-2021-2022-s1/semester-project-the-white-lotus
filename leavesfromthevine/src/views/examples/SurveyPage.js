02319192938ac011477fab6f14e86b39ca9a2e19
Jomar-Santos <jomar.santos@upr.edu>
2021-02-24 12:23:38 -0400

    Finished a base for the survey page and once options are submited then redirected to landing-page

--- leavesfromthevine/src/views/examples/SurveyPage.js
+++ leavesfromthevine/src/views/examples/SurveyPage.js
@@ -29,6 +29,11 @@ import {
   Nav,
   TabContent,
   TabPane,
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
   Container,
   Row,
   Col,
@@ -61,134 +66,70 @@ function ProfilePage() {
       <SurveyPageHeader />
       <div className="section profile-content">
         <Container>
-          {/* <div className="owner">
-            <div className="avatar">
-              <img
-                alt="..."
-                className="img-circle img-no-padding img-responsive"
-                src={require("assets/img/faces/joe-gardner-2.jpg")}
-              />
-            </div>
-            <div className="name">
-              <h4 className="title">
-                Jane Faker <br />
-              </h4>
-              <h6 className="description">Music Producer</h6>
-            </div>
-          </div> */}
-          <br/>
-          <br/>
-          <Row>
-            <Col className="ml-auto mr-auto text-center" md="6">
-              <p>
-                This small survey will provide us with information
-                which we would use to provide teas which you might enjoy.
-              </p>
-              {/* <br />
-              <Button className="btn-round" color="default" outline>
-                <i className="fa fa-cog" /> Settings
-              </Button> */}
-            </Col>
-          </Row>
           <br />
-          {/* <div className="nav-tabs-navigation">
-            <div className="nav-tabs-wrapper">
-              <Nav role="tablist" tabs>
-                <NavItem>
-                  <NavLink
-                    className={activeTab === "1" ? "active" : ""}
-                    onClick={() => {
-                      toggle("1");
-                    }}
-                  >
-                    Follows
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    className={activeTab === "2" ? "active" : ""}
-                    onClick={() => {
-                      toggle("2");
-                    }}
-                  >
-                    Following
-                  </NavLink>
-                </NavItem>
-              </Nav>
-            </div>
-          </div> */}
-          {/* Tab panes */}
-          {/* <TabContent className="following" activeTab={activeTab}>
-            <TabPane tabId="1" id="follows">
-              <Row>
-                <Col className="ml-auto mr-auto" md="6">
-                  <ul className="list-unstyled follows">
-                    <li>
-                      <Row>
-                        <Col className="ml-auto mr-auto" lg="2" md="4" xs="4">
-                          <img
-                            alt="..."
-                            className="img-circle img-no-padding img-responsive"
-                            src={require("assets/img/faces/clem-onojeghuo-2.jpg")}
-                          />
-                        </Col>
-                        <Col className="ml-auto mr-auto" lg="7" md="4" xs="4">
-                          <h6>
-                            Flume <br />
-                            <small>Musical Producer</small>
-                          </h6>
-                        </Col>
-                        <Col className="ml-auto mr-auto" lg="3" md="4" xs="4">
-                          <FormGroup check>
-                            <Label check>
-                              <Input
-                                defaultChecked
-                                defaultValue=""
-                                type="checkbox"
-                              />
-                              <span className="form-check-sign" />
-                            </Label>
-                          </FormGroup>
-                        </Col>
-                      </Row>
-                    </li>
-                    <hr />
-                    <li>
-                      <Row>
-                        <Col className="mx-auto" lg="2" md="4" xs="4">
-                          <img
-                            alt="..."
-                            className="img-circle img-no-padding img-responsive"
-                            src={require("assets/img/faces/ayo-ogunseinde-2.jpg")}
-                          />
-                        </Col>
-                        <Col lg="7" md="4" xs="4">
-                          <h6>
-                            Banks <br />
-                            <small>Singer</small>
-                          </h6>
-                        </Col>
-                        <Col lg="3" md="4" xs="4">
-                          <FormGroup check>
-                            <Label check>
-                              <Input defaultValue="" type="checkbox" />
-                              <span className="form-check-sign" />
-                            </Label>
-                          </FormGroup>
-                        </Col>
-                      </Row>
-                    </li>
-                  </ul>
-                </Col>
-              </Row>
-            </TabPane>
-            <TabPane className="text-center" tabId="2" id="following">
-              <h3 className="text-muted">Not following anyone yet :(</h3>
-              <Button className="btn-round" color="warning">
-                Find artists
+          <br />
+          <Card>
+            <Row>
+              <Col className="ml-auto mr-auto text-center" md="6">
+                <p>
+                  This small survey will provide us with information
+                  which we would use to provide teas which you might enjoy.
+              </p>
+                <CardBody>
+                  <CardTitle></CardTitle>
+                  <CardSubtitle></CardSubtitle>
+                  <CardText>
+                    <FormGroup check>
+                      <Label check>
+                        <Input defaultValue="" type="checkbox" />
+                  Option <span className="form-check-sign" />
+                      </Label>
+                      <Label check>
+                        <Input defaultValue="" type="checkbox" />
+                  Option <span className="form-check-sign" />
+                      </Label>
+                    </FormGroup>
+                  </CardText>
+                </CardBody>
+              </Col>
+            </Row>
+            <br />
+            <Row>
+              <Col className="ml-auto mr-auto text-center" md="6">
+                <CardBody>
+                  <CardTitle></CardTitle>
+                  <CardSubtitle></CardSubtitle>
+                  <CardText>
+                    <FormGroup check>
+                      <Label check>
+                        <Input defaultValue="" type="checkbox" />
+                  Option <span className="form-check-sign" />
+                      </Label>
+                      <Label check>
+                        <Input defaultValue="" type="checkbox" />
+                  Option <span className="form-check-sign" />
+                      </Label>
+                    </FormGroup>
+                  </CardText>
+                </CardBody>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col className="ml-auto mr-auto" md="1">
+                <Button
+                  className="btn-round mr-1"
+                  color="primary"
+                  href="/landing-page"
+                >
+                  Submit
               </Button>
-            </TabPane>
-          </TabContent> */}
+              </Col>
+            </Row>
+            <br />
+            <br />
+
+          </Card>
         </Container>
       </div>
       <DemoFooter />
